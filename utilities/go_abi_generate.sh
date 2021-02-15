438e652de1f45c98f51dd58d40a7191ea042f79e
JulianHLB <julian.caille@humbleluckball.com>
2021-02-15 18:04:49 +0100

    feat(abi): changed utility to generate lowercase pkgs and updated abis

--- utilities/go_abi_generate.sh
+++ utilities/go_abi_generate.sh
@@ -2,6 +2,7 @@
 
 contract_name=$1
 contract_sanitized=$(echo "$contract_name" | rev | cut -d / -f1 | rev |  cut -d . -f1)
+contract_lowercase=$(echo "$contract_sanitized" | awk '{print tolower($0)}')
 curdir=$(pwd)
 
-abigen --sol="$contract_name" --pkg="$contract_sanitized" --out="$curdir"/abi/golang/"$contract_sanitized".go
+abigen --sol="$contract_name" --pkg="$contract_lowercase" --out="$curdir"/abi/golang/"$contract_lowercase".go
