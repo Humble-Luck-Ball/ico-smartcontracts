3c941b16958111b99e3eb058c6834e5f88bdf91e
JulianHLB <julian.caille@humbleluckball.com>
2021-02-17 15:47:11 +0100

    chore(hlbico test): adapted test to test service-blockchain-ico

--- test/ico/hlbico.js
+++ test/ico/hlbico.js
@@ -20,44 +20,23 @@ contract("HLBICO", accounts => {
             accounts[0]
         );
 
-        await web3.eth.sendTransaction({value: web3.utils.toWei("0.5", "ether"), from: accounts[1], to: addrToWhitelist})
+        // await web3.eth.sendTransaction({value: web3.utils.toWei("0.5", "ether"), from: accounts[1], to: addrToWhitelist})
 
         await ico.changeToken(lbc.address);
-    });
-
-    it("should whitelist", async () => {
-        let ico = await HLBICO.deployed();
-
-
-        let isWhitelisted = await ico.isWhitelisted(accounts[0]);
-
-        assert.equal(isWhitelisted.toString(), "0", `Account ${accounts[0]} was already whitelisted when it shouldn't be`);
-
-        await ico.addWhitelistedRegistered(accounts[0]);
-
-        isWhitelisted = await ico.isWhitelisted(accounts[0]);
-
-        assert.equal(isWhitelisted.toString(), "1", `Account ${accounts[0]} wasn't whitelisted properly for registered`);
-
-        await ico.addWhitelistedKYC(accounts[0]);
-
-        isWhitelisted = await ico.isWhitelisted(accounts[0]);
-
-        assert.equal(isWhitelisted.toString(), "2", `Account ${accounts[0]} wasn't whitelisted properly for KYC`);
     });
 
     it("should be able to invest", async () => {
         let ico = await HLBICO.deployed();
 
-        await ico.buyTokens(accounts[0], {value: web3.utils.toWei("0.5", "ether"), from: accounts[0]})
+        await ico.buyTokens(accounts[2], {value: web3.utils.toWei("0.5", "ether"), from: accounts[2]})
 
-        let accountBalance = await ico.balanceOf(accounts[0]);
+        let accountBalance = await ico.balanceOf(accounts[2]);
 
         // for 1eth = 1000€
         assert.equal(accountBalance.toString(), web3.utils.toWei((4750 * 0.5).toString(), "ether").toString(), "Account's balance should be 4750 LBC");
     });
 
-    it("Should adjust by 4%", async () => {
+    xit("Should adjust by 4%", async () => {
         let ico = await HLBICO.deployed();
 
         await ico.adjustEtherValue(new web3.utils.BN("960"))
